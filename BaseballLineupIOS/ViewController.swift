b93fe7f0bb1272b4809c2246c0c4b1dc64558bd0
koro <koro.nn99@gmail.com>
2020-11-02 07:47:11 -0800

    Initial Commit

---
+++ BaseballLineupIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseballLineupIOS
+//
+//  Created by Koro Saka on 2020-11-02.
+//  Copyright © 2020 Koro Saka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
