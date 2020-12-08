3216ddec6a08dd77146204f08d1387dddb58573f
koro <koro.nn99@gmail.com>
2020-12-08 01:18:02 -0800

    created basic screen design

--- BaseballLineupIOS/View/ViewController/SubMemberViewController.swift
+++ BaseballLineupIOS/View/ViewController/SubMemberViewController.swift
@@ -13,6 +13,7 @@ class SubMemberViewController: UIViewController {
     var viewModel: SubMemberViewModel?
     var parentViewModel: CustomTabBarViewModel?
     
+    @IBOutlet weak var subPlayerTable: UITableView!
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -30,5 +31,44 @@ class SubMemberViewController: UIViewController {
     
     func setup() {
         viewModel = .init()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        subPlayerTable.dataSource = self
+    }
+}
+
+extension SubMemberViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let subTableCell = tableView.dequeueReusableCell(withIdentifier: "SubPlayerTableCell", for: indexPath) as? SubPlayerTableCell else {
+            fatalError("Could not create ReviewCell")
+        }
+        
+        
+        subTableCell.subButton.backgroundColor = .systemBlue
+        subTableCell.subButton.layer.cornerRadius = 15
+        subTableCell.subButton.layer.borderColor = UIColor.black.cgColor
+        subTableCell.subButton.layer.borderWidth = 2
+        
+        designRoleLabel(uiLabel: subTableCell.pitcherLabel)
+        designRoleLabel(uiLabel: subTableCell.hitterLabel)
+        designRoleLabel(uiLabel: subTableCell.runnerLabel)
+        designRoleLabel(uiLabel: subTableCell.fielderLabel)
+        
+        subTableCell.nameText.text = "テスト太郎"
+        return subTableCell
+    }
+    
+    func designRoleLabel(uiLabel: UILabel) {
+        uiLabel.layer.cornerRadius = 5
+        uiLabel.clipsToBounds = true
+        uiLabel.layer.borderColor = UIColor.black.cgColor
+        uiLabel.layer.borderWidth = 0.7
     }
 }
---
+++ BaseballLineupIOS/View/ViewController/SubPlayerTableCell.swift
@@ -0,0 +1,23 @@
+//
+//  SubPlayerTableCell.swift
+//  BaseballLineupIOS
+//
+//  Created by Koro Saka on 2020-12-07.
+//  Copyright © 2020 Koro Saka. All rights reserved.
+//
+
+import UIKit
+class SubPlayerTableCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var pitcherLabel: UILabel!
+    @IBOutlet weak var hitterLabel: UILabel!
+    @IBOutlet weak var fielderLabel: UILabel!
+    @IBOutlet weak var runnerLabel: UILabel!
+    @IBOutlet weak var nameText: UILabel!
+    @IBOutlet weak var subButton: UIButton!
+    @IBAction func onClickSUb(_ sender: Any) {
+    }
+    
+    
+}
