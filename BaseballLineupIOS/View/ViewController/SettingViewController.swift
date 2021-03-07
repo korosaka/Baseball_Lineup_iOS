f651da916058156fa5d685a05e078ef3bbf5e6d4
koro <koro.nn99@gmail.com>
2021-03-06 19:53:41 -0800

    add a modal view

---
+++ BaseballLineupIOS/View/ViewController/SettingViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SettingViewController.swift
+//  BaseballLineupIOS
+//
+//  Created by Koro Saka on 2021-03-06.
+//  Copyright © 2021 Koro Saka. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7)
+    }
+    
+    @IBAction func onClickClose(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
